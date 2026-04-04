869f0ec1202ac1be3c1536a99c1a116e5892f429
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-04 22:41:12 +0400

    new tasks

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]  # skip the script name
+    count = len(args)
+
+    if count == 0:
+        print("0 arguments.")
+    else:
+        if count == 1:
+            print("1 argument:")
+        else:
+            print(f"{count} arguments:")
+
+        for i in range(count):
+            print(f"{i + 1}: {args[i]}")
