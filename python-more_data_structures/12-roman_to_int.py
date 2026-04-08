7e87dace40c688e44e45e7637b76aa7388e1ddc8
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-08 19:17:45 +0400

    tasks

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     alp={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500}
     r=0
     for i in range(len(roman_string)-1):
