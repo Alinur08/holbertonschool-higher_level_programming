74f7f8e252d9bb98f5e500b64e532cea55b2794c
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-11 21:38:05 +0400

    new tasks

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -71,4 +71,18 @@ class Rectangle:
         """Print a message when an instance is deleted."""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
-    
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        Compare two rectangles and return the one with the larger area.
+
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
