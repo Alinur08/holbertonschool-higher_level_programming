531f95965f73ef15dcfe6cf19659db3e41cc4627
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-11 16:37:17 +0400

    new tasks

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
 class Square:
+    """Class that defines a square."""
+
     def __init__(self, size):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size: The size of the square's side.
+        """
         self.__size = size
