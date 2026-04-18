a01d949333ba5608e9c8f73edbefc5273920f1e9
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-18 17:45:16 +0400

    new tasks

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for exact class comparison."""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if an object is an instance of a specified class.
+    """
+    return isinstance(obj) is a_class
