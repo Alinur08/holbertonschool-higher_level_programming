247d707d2609abf0544c9ceac3d31052b3bdc15b
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-18 17:54:12 +0400

    new tasks

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """
-    Checks if an object is an instance of a class that inherited 
+    Checks if an object is an instance of a class that inherited
     (directly or indirectly) from the specified class.
     """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
