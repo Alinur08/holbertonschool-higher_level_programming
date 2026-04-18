d2e310a073f6fd630052db36eefe1ea7b3f86c52
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-18 17:28:03 +0400

    new tasks

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,4 +3,13 @@
 
 
 def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        list: A list of strings representing attributes and methods.
+    """
     return dir(obj)
