c81a9faf7c0a1d12b36df2e40d606071683f10fd
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-18 18:24:00 +0400

    new tasks

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that defines a square, inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """
+        Initialize a new Square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Returns the square description."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
