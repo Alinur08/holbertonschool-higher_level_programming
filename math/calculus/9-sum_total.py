f2761d1da780f611fef6a54d95318dfbc1be0a92
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-25 00:33:24 +0400

    math

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-def count_down(n):
+def summation_i_squared(n):
     if n < 0:
         return None
     if n == 0 or n == 1:
         return 1    
-    return count_down(n - 1)*n
+    return summation_i_squared(n - 1)*n
