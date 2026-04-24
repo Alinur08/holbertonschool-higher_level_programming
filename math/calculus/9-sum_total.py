c82ba43c7e1392dc37d36facdbe4510fde0df9bb
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-25 00:35:30 +0400

    math

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,6 +2,7 @@
 def summation_i_squared(n):
     if n < 0:
         return None
-    if n == 0 or n == 1:
-        return 1    
-    return summation_i_squared(n - 1) * n ** 2
+    if n == 0:
+        return 0
+    # Add n squared to the sum of the previous squares
+    return summation_i_squared(n - 1) + (n ** 2)
