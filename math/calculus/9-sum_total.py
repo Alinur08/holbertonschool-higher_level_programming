2429a8b87d605ca314fca966540d7a35db7c0aaa
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-25 00:27:32 +0400

    math

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,7 +2,9 @@
 def summation_i_squared(n):
     if isinstance(n, int):
         s = 0
-        for i in range(1, n+1):
+        i = 1
+        while i <= n:
             s += i**2
+            i += 1
         return s
     return None
