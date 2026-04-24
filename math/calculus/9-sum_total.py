da44fb15c6aa6c78ef94ddca48d32ba160e0b8e6
zamanli@fedora <alinurzamanli08@gmail.com>
2026-04-25 00:31:48 +0400

    math

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def count_down(n):
-    if not isinstance(n, int):
-        return
+    if n < 0:
+        return None
     if n == 0 or n == 1:
         return 1    
     return count_down(n - 1)*n
