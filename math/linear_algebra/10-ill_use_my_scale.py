439e96fade12db5b3167d644ac9b6059d4e0cc16
zamanli@fedora <alinurzamanli08@gmail.com>
2026-05-02 17:06:09 +0400

    math

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -5,11 +5,5 @@
 def np_shape(matrix):
     """
     Calculates the shape of a numpy.ndarray.
-    
-    Args:
-        matrix (numpy.ndarray): The array to check.
-        
-    Returns:
-        tuple: The dimensions of the matrix.
     """
     return matrix.shape
