233a38a8f17d39e6854229433863effc4f466d26
zamanli@fedora <alinurzamanli08@gmail.com>
2026-05-02 16:52:27 +0400

    math

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -7,11 +7,11 @@ def mat_mul(mat1, mat2):
     if len(mat1[0]) != len(mat2):
         return None
     mat = []
-    for i in range(mat1):
+    for i in range(len(mat1)):
         row = []
-        for k in range(mat2[0]):
+        for k in range(len(mat2[0])):
             s = 0
-            for j in range(mat1[0]):
+            for j in range(len(mat1[0])):
                 s += mat1[i][j] * mat2[j][k]
             row.append(s)
             
