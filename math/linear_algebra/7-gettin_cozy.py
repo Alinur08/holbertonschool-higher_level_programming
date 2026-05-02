9bfd40888e5403d97714c8be55aa6621a56269b6
zamanli@fedora <alinurzamanli08@gmail.com>
2026-05-02 16:32:50 +0400

    math

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -12,7 +12,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     elif axis == 1:
         if len(mat1) != len(mat2):
             return None
-        mat.append(mat1[i]+mat2[i] for i in range(len(mat1))) 
+        mat = [mat1[i] + mat2[i] for i in range(len(mat1))] 
     else:
         return None
     return mat
