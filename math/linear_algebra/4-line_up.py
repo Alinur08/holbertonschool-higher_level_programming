162aa4f1bad922fcb6902ecc327139ab0dce7aa1
zamanli@fedora <alinurzamanli08@gmail.com>
2026-05-02 15:59:33 +0400

    math

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -7,6 +7,6 @@ def add_arrays(arr1, arr2):
     if len(arr1) != len(arr2):
         return None
     arr = []
-    for i in len(arr1):
+    for i in range(len(arr1)):
         arr.append(arr1[i] + arr2[i])
     return arr
