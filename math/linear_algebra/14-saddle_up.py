2a202edc6be7c48090c82fc2dfb5a988582f0747
zamanli@fedora <alinurzamanli08@gmail.com>
2026-05-02 17:17:44 +0400

    math

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for matrix multiplication using NumPy"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication on two numpy.ndarrays.
+    """
+    return np.matmul(mat1, mat2)
