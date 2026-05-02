15082fad921aefaa0d10c07dda5d7c0ad0db13aa
zamanli@fedora <alinurzamanli08@gmail.com>
2026-05-02 15:58:49 +0400

    math

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module to perform matrix operations"""
+
+
+def add_arrays(arr1, arr2):
+    """Adding arrays"""
+    if len(arr1) != len(arr2):
+        return None
+    arr = []
+    for i in len(arr1):
+        arr.append(arr1[i] + arr2[i])
+    return arr
