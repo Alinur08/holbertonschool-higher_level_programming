f23bc85a433517b82fc72247a136d3408724228a
zamanli@fedora <alinurzamanli08@gmail.com>
2026-05-02 16:51:21 +0400

    math

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module to perform matrix operations"""
+
+
+def mat_mul(mat1, mat2):
+    """Multi Mat"""
+    if len(mat1[0]) != len(mat2):
+        return None
+    mat = []
+    for i in range(mat1):
+        row = []
+        for k in range(mat2[0]):
+            s = 0
+            for j in range(mat1[0]):
+                s += mat1[i][j] * mat2[j][k]
+            row.append(s)
+            
+        mat.append(row)
+    return mat
