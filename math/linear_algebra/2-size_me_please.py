cb0a0f94aaf8de3ad6398bd5b6fec72631d5f521
zamanli@fedora <alinurzamanli08@gmail.com>
2026-05-02 15:47:39 +0400

    math

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+"""Return matrix shape"""
 def matrix_shape(matrix):
-    """Return matrix shape"""
     shape = []
     while isinstance(matrix, list) and len(matrix) != 0:
         shape.append(len(matrix))
