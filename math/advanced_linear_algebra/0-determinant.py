baf988d0bf9a79dd70084a86ccb8282372a1636f
zamanli@fedora <alinurzamanli08@gmail.com>
2026-05-09 21:06:42 +0400

    math

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -15,7 +15,7 @@ def determinant(matrix):
     if n == 1:
         return matrix[0][0]
     if n == 2:
-        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][2])
+        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
     for j in range(n):
         minor = [row[:j] + row[j+1:] for row in matrix[1:]]
         sign = (-1) ** j
